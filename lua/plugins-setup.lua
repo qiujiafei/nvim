0cf864db5b5c326d34f4d4ecf3209f5c2eb6570c
gaffey <253896514@qq.com>
2022-12-22 17:21:53 +0800

    feat: add fidget plugin

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -94,6 +94,7 @@ return packer.startup(function(use)
 	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
 	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
 	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	use("j-hui/fidget.nvim")
 
 	-- formatting & linting
 	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
